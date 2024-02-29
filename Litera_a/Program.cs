7af7dffb6bfc1dbca6acca780ddc4ad5bbaa8a37
BartlomiejPPG <bartlomiejnedzi2006@gmail.com>
2024-02-29 09:46:33 +0100

    add new files

---
+++ Litera_a/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace imienazwisko
+{
+    class zadanie1
+    {
+        static void Main(string[] args)
+        {
+            string plik = @"C:\test\test_jakub_wanat.txt";
+            string nazwa = Path.GetFileName(plik);
+            int countA = CountOccurrences(nazwa, 'a');
+            Console.WriteLine(countA);
+        }
+        static int CountOccurrences(string text, char target)
+        {
+            int count = 0;
+            foreach (char c in text)
+            {
+                if (c == target)
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+}
